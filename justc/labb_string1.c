63408c9de50049a6e6c7f8473b3ca0203cb48aec
Conny Hansson <conhan@hotmail.se>
2020-10-15 18:36:31 +0200

    Added comments

--- justc/labb_string1.c
+++ justc/labb_string1.c
@@ -45,9 +45,21 @@ void labb1(void)
 		}
 	}
 	char added_strings[STRLEN * 3] = "";
+	/*
+		String three times the length of STRLEN because it's going to hold three strings
+		of up to length STRLEN
+
+		Also initialized because we are going to be concatenating the strings(adding to it)
+		onto this one and if we don't we'll end up adding the other strings onto
+		whatever happened to be in place where we declared added_strings
+	*/
 	for (int row = 0; row < 3; row++)
 	{
 		strcat(added_strings, string1[row]);
+		/*
+			strcat(string concatenate) works exactly like "string1 + string2" in Python
+			it just adds the strings together. Also works with string literals
+		*/
 	}
 	printf("\nconcatenated: %s", added_strings);
 }
@@ -58,10 +70,15 @@ void labb2(void)
 		b. Print ut the position of "w"
 */
 {
-	char hello_world[] = "Hello, world!";
+	char hello_world[] = "Hello, world!"; // don't have to specify length when initializing strings to literals
 
 	for (int index = 0; index < strlen(hello_world); index++)
 		if (hello_world[index] == 'w') printf("\nPosition is: %d", index);
+	/*
+		A short for loop that loops through the elements of "hello_world"
+		with an if statement that checks if the current element is the char 'w'
+		and prints ut the position if true "index == position"
+	*/
 }
 void labb3(void)
 /*
@@ -72,7 +89,7 @@ void labb3(void)
 		d. Modify the string to "i Am A C hAcKeR"
 */
 {
-	char b[] = "I am a C hacker";
+	char b[] = "I am a C hacker"; // don't have to specify length when initializing strings to literals
 
 	for (int index = strlen(b); index >= 0; index--)
 	{
@@ -142,13 +159,13 @@ void labb4(void)
 		Result should be "Kurt Andersson"
 */
 {
-	char name[] = "kurt andersson";
-	int capitalize = 1;
+	char name[] = "kurt andersson"; // don't have to specify length when initializing strings to literals
+	int capitalize = 1; // remember 1 is evaluated as "True" and 0 as "False"
 	for (int index = 0; index < strlen(name); index++)
 	{
-		if (capitalize) name[index] = toupper(name[index]);
-		if (name[index] == ' ') capitalize = 1;
-		else capitalize = 0;
+		if (capitalize) name[index] = toupper(name[index]); // if capitalize is true use "toupper" to change element to upper case
+		if (name[index] == ' ') capitalize = 1; // this will make the loop change the next element to upper case
+		else capitalize = 0; // sets capitalize to false for all other letters
 	}
 	printf("\n%s", name);
 
@@ -160,15 +177,15 @@ void labb5(void)
 		Calculate the amount of "*" characters in the string
 */
 {
-	char string[] = "this is the string you will change";
-	int count = 0;
+	char string[] = "this is the string you will change"; // don't have to specify length when initializing strings to literals
+	int count = 0; // this variable will be used to count the number of "*" chars in the string
 	for (int index = 0; index < strlen(string); index++)
 	{
-		if (string[index] == '*') count++;
+		if (string[index] == '*') count++; // increment count when "*" found
 		if (string[index] == ' ')
 		{
-			string[index] = '*';
-			count++;
+			string[index] = '*'; // changes " " to "*"
+			count++; // increments count when " " found, which will be changed to "*"
 		}
 	}
 	printf("\nModified string: %s\tcount of * chars: %d", string, count);
@@ -181,8 +198,19 @@ void labb6(void)
 		put each number into a separate column in the array.
 */
 {
-	char string[] = "one,two,three,four,five,six,seven";
+	char string[] = "one,two,three,four,five,six,seven"; // don't have to specify length when initializing strings to literals
 	char multidimensional[7][STRLEN];
+
+	/*
+		This whole function works like labb 3 so check that for an explanation of strtok()
+		
+		The difference is is that instead of printing the separated strings we're adding
+		them onto a multidimensional(one dimension, rows for strings and the other, col for
+		the contents of the strings. Think like a matrix)
+
+		We're using strcpy(string copy) for this which copies the string "number" is pointing
+		to onto the array.
+	*/
 
 	char delimiter[] = ",";
 
@@ -216,10 +244,10 @@ void labb7(void)
 
 	for (int index = 0; index < strlen(mail_address); index++)
 	{
-		if (mail_address[index] == '@') foundat++;
-		if (mail_address[index] == '.') foundpunctuation++;
+		if (mail_address[index] == '@') foundat++; // increments "foundat" so it is evaluated as "True"
+		if (mail_address[index] == '.') foundpunctuation++; // same for this but I'm kinda cheaty using this to count char after "." too
 		if (foundpunctuation) foundpunctuation++;
 	}
-	if (foundat && foundpunctuation > 3) printf("\nAddress is correct");
+	if (foundat && foundpunctuation > 3) printf("\nAddress is correct"); // if "@" is present and(&&) "." with at least 2 trailing chars
 	else printf("\nIncorrect Address");
 }
