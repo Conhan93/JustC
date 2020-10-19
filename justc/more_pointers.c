8f64778034a9e5faf3840d9e8fea636c99131555
Conny Hansson <conhan@hotmail.se>
2020-10-19 18:40:48 +0200

    added a few more examples

--- justc/more_pointers.c
+++ justc/more_pointers.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 
 
 
 
-
+// function prototypes
 void print_int(int number);
+void print_array(int* arr, int arr_size);
+bool over18(int age);
 
 
 
@@ -49,8 +52,38 @@ int main(void)
 	// calling function "print_int" via pointer with function argument "10"
 	(*func_ptr)(10);
 
+	// new function pointer
+	void (*func2_ptr)(int*, int) = &print_array;
+
+	// calling new function pointer
+	(*func2_ptr)(arr, sizeof(arr) / sizeof(int));
+
+	// declaring boolean function pointer
+	bool (*func3_ptr)(int) = &over18;
+
+	// declaring & initalizing int
+	int age = 0;
+
+	// enter age
+	printf("\nEnter age: ");
+	scanf("%d", &age);
+
+	// calling function pointer to check condition
+	if ((*func3_ptr)(age)) printf("\nOver 18");
+	else printf("\n18 or under");
+
 }
 void print_int(int number)
 {
 	printf("\nprinting int: %d", number);
+}
+void print_array(int* arr, int arr_size)
+{
+	printf("\n");
+	for (int index = 0; index < arr_size; index++, arr++) printf("%d ", *arr);
+}
+bool over18(int age)
+{
+	if (age > 18) return true;
+	else return false;
 }
