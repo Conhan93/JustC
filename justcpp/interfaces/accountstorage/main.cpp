c92cf13e69ca42a4f9f024e82e3eb9d442bfb9b7
Conny Hansson <conhan@hotmail.se>
2020-12-10 16:49:10 +0100

    done

--- justcpp/interfaces/accountstorage/main.cpp
+++ justcpp/interfaces/accountstorage/main.cpp
@@ -24,30 +24,55 @@ class Bank
     }
   	bool AddAccount(std::string id)
     {
+        // uses functions declared in interface class! overriden
+        // by implementation, uses implementation method
         accountStorage->AddAccount(id);
  	      return true;
     }
   	Account *GetAccount(std::string id)
     {
+      // uses functions declared in interface class! overriden
+      // by implementation, uses implementation method
       return accountStorage->GetAccount(id);
     }
   private:
   	IAccountStorage* accountStorage;
+
+    /*
+      Interface class pointers are able to point to implementations
+      of interface.
+
+      IAccountStorage pointer is able to point to any class inheriting
+      from it, vectorStorage, MapAccountStorage and multivectorstorage.
+
+      so bank can support any implementation of IAccountStorage without
+      having to know anything about that implementation!
+
+      Also since it is a pointer, it can be reassigned. Bank can change
+      storage method while the program is still running just by reassigning
+      the pointer to a different implementation!(not possible with references)
+    */
 };
 
 
 int main()
 {
+  // alias makes for prettier code
   using clock = std::chrono::high_resolution_clock;
   clock::time_point start;
   clock::time_point end;
 
+
+  // implementations of IAccountStorage!! polymorphisms
   VectorAccountStorage *storage = new VectorAccountStorage();
   //MapAccountStorage *storage = new MapAccountStorage();
   //MultiDistributedVectorAccountStorage *storage = new MultiDistributedVectorAccountStorage();
+
+
+
   Bank bank = Bank(storage);
 
-  for(int i = 0; i < 100000; i++)
+  for(int i = 0; i < 1000000; i++)
     bank.AddAccount(std::to_string(i));
 
   std::string accountNr;
--- justcpp/interfaces/accountstorage/mapstorage/MapAccountStorage.cpp
+++ justcpp/interfaces/accountstorage/mapstorage/MapAccountStorage.cpp
@@ -4,17 +4,34 @@
 
 MapAccountStorage::~MapAccountStorage()
 {
+  // alias
   using mapIter = mapStorage::const_iterator;
 
   for (mapIter it = accounts.begin(); it != accounts.end(); it++)
 		delete  it->second;
+
+    /*
+        Deconstructor called at end of object lifetime.
+
+        this one loops through all accounts and frees the
+        allocated memory for them(since they're created using new)
+    */
 }
 void MapAccountStorage::AddAccount(std::string id)
 {
   Account *account = new Account(id);
 	accounts[id] = account;
+  /*
+      Maps work just like dictionaries in Python,
+      except that you have to declare types for keys
+      and values when declaring a map.
+
+      here string ID is used as a key and account
+      is saved as value
+  */
 }
 Account* MapAccountStorage::GetAccount(std::string id)
 {
+  // fetches, value at key - the account with the ID key
   return accounts[id];
 }
